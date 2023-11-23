6f3e206c1b9ed34dba33a498374677a34f562acd
AlexPolyak2003 <polyanichko03@inbox.ru>
2023-11-23 18:46:49 +0300

    m

--- src/components/Videoplayer/Videoplayer.component.jsx
+++ src/components/Videoplayer/Videoplayer.component.jsx
@@ -1,6 +1,5 @@
 import styles from "./Videoplayer.module.scss";
-import video from "../../../src/videos/Video_2.mp4";
-import video_mobile from "../../../src/videos/m2ts.m2ts";
+import video from "../../../src/videos/videofon.mp4";
 import VideoHeader from "../VideoHeader/VideoHeader.component";
 import SliderComponent from "../Slider/Slider_desktop/Slider.component";
 import SliderMobile from "../Slider/Slider_mobile/Slider_mobile.component";
@@ -29,7 +28,6 @@ const Videoplayer = () => {
 
         <video loop autoPlay muted playsinline>
           <source src={video} type="video/mp4" />
-          {/* <source src={video_mobile} type="video/m2ts" /> */}
         </video>
       </div>
     </div>
