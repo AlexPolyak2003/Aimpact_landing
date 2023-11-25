2ee61952556afe557f6eac58e0d9c0de91e8f234
AlexPolyak2003 <polyanichko03@inbox.ru>
2023-11-26 02:53:11 +0300

    m

--- src/components/MainPage/MainPage.component.jsx
+++ src/components/MainPage/MainPage.component.jsx
@@ -1,30 +1,25 @@
 import styles from "./MainPage.module.scss";
-import { lazy } from "react";
+import React from "react";
 import AboutUs from "../Pages/AboutUs/AboutUs.component";
-import Direction_page from "../Pages/Directions_Industries/Directions/Directions.component";
-import Industry from "../Pages/Directions_Industries/Industries/Industry.component";
 import Project from "../Pages/Projects/Project.component";
 import ActualProject from "../Pages/ActualProjects/ActualProject.component";
 import Footer from "../Footer/Footer.component";
-import Videoplayer from "../Videoplayer/Videoplayer.component";
+// import Videoplayer from "../Videoplayer/Videoplayer.component";
 import Main from "../Pages/Directions_Industries/Main.component";
 
-const MyLazyComponent = lazy(
-  () =>
-    new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(import("../Videoplayer/Videoplayer.component"));
-      }, 3000);
-    })
+const MyLazyComponent = React.lazy(() =>
+  import("../Videoplayer/Videoplayer.component")
 );
 
 const MainPage = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <MyLazyComponent>
-          <Videoplayer />
-        </MyLazyComponent>
+        {/* <MyLazyComponent> */}
+        <React.Suspense>
+          <MyLazyComponent />
+        </React.Suspense>
+        {/* </MyLazyComponent> */}
         <AboutUs />
         <Main />
         <Project />
