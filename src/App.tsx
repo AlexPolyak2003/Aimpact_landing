4d3d7f12200c8e582e7b2f15d294e011618ea3d7
AlexPolyak2003 <polyanichko03@inbox.ru>
2023-11-28 21:55:23 +0300

    m

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 10000);
   }, []);
   return (
     <div className={styles.wrapper}>
