1fd552de513497a713e71f8bbe79894d11f46058
AscentDP <podvalov.dima@mail.ru>
2023-12-02 22:59:03 +0300

    пара анимаций и тэги

--- src/components/Pages/Directions_Industries/Main.component.jsx
+++ src/components/Pages/Directions_Industries/Main.component.jsx
@@ -1,20 +1,20 @@
-import styles from "./Main.module.scss";
-import { useState, useRef } from "react";
-import Direction_page from "./Directions/Directions.component";
-import Industry from "./Industries/Industry.component";
-import mozg from "../../../icons/Directions/mozg.svg";
+import styles from './Main.module.scss';
+import { useState, useRef } from 'react';
+import Direction_page from './Directions/Directions.component';
+import Industry from './Industries/Industry.component';
+import mozg from '../../../icons/Directions/mozg.svg';
 
 const Main = () => {
-  const button_1 = document.getElementById("btn_1");
-  const button_2 = document.getElementById("btn_2");
+  const button_1 = document.getElementById('btn_1');
+  const button_2 = document.getElementById('btn_2');
   const [firstState, setFirstState] = useState(true);
   const [secondState, setSecondState] = useState(false);
 
   const [isActive, setIsActive] = useState(false);
 
-  const [color, setColor] = useState(["#b0bde6", "#2d418d"]);
-  const [underlineColor, setUnderlineColor] = useState(["#2d418d", "#b0bde6"]);
-  const [underline, setUnderline] = useState(["underline", "none"]);
+  const [color, setColor] = useState(['#b0bde6', '#2d418d']);
+  const [underlineColor, setUnderlineColor] = useState(['#2d418d', '#b0bde6']);
+  const [underline, setUnderline] = useState(['underline', 'none']);
 
   const toggleFirstContent = () => {
     setIsActive(true);
@@ -38,18 +38,18 @@ const Main = () => {
 
   const handleClick = () => {
     if (!firstState) {
-      setColor(["#b0bde6", "#2d418d"]);
-      setUnderlineColor(["#b0bde6", "#2d418d"]);
-      setUnderline(["underline", "none"]);
+      setColor(['#b0bde6', '#2d418d']);
+      setUnderlineColor(['#b0bde6', '#2d418d']);
+      setUnderline(['underline', 'none']);
       toggleFirstContent();
     }
   };
 
   const handleClick_2 = () => {
     if (!secondState) {
-      setColor(["#2d418d", "#b0bde6"]);
-      setUnderlineColor(["#2d418d", "#b0bde6"]);
-      setUnderline(["none", "underline"]);
+      setColor(['#2d418d', '#b0bde6']);
+      setUnderlineColor(['#2d418d', '#b0bde6']);
+      setUnderline(['none', 'underline']);
       toggleSecondContent();
     }
   };
@@ -60,25 +60,23 @@ const Main = () => {
         <div className={styles.left_box}>
           <div className={styles.titles}>
             <h1
-              style={{ color: color[1], "text-decoration-line": underline[0] }}
+              style={{ color: color[1], 'text-decoration-line': underline[0] }}
               id="btn_1"
               ref={button_1}
               onClick={handleClick}
-              className={styles.title_1}
-            >
+              className={styles.title_1}>
               направления
             </h1>
 
             <h1
               style={{
                 color: color[0],
-                "text-decoration-line": underline[1],
+                'text-decoration-line': underline[1],
               }}
               id="btn_2"
               ref={button_2}
               onClick={handleClick_2}
-              className={styles.title_2}
-            >
+              className={styles.title_2}>
               индустрии
             </h1>
           </div>
@@ -96,25 +94,23 @@ const Main = () => {
         <div className={styles.left_box}>
           <div className={styles.titles}>
             <h1
-              style={{ color: color[1], "text-decoration-line": underline[0] }}
+              style={{ color: color[1], 'text-decoration-line': underline[0] }}
               id="btn_1"
               ref={button_1}
               onClick={handleClick}
-              className={styles.title_1}
-            >
+              className={styles.title_1}>
               направления
             </h1>
 
             <h1
               style={{
                 color: color[0],
-                "text-decoration-line": underline[1],
+                'text-decoration-line': underline[1],
               }}
               id="btn_2"
               ref={button_2}
               onClick={handleClick_2}
-              className={styles.title_2}
-            >
+              className={styles.title_2}>
               индустрии
             </h1>
           </div>
--- src/components/VideoHeader/VideoHeader.component.tsx
+++ src/components/VideoHeader/VideoHeader.component.tsx
@@ -1,8 +1,8 @@
-import React, { ReactElement } from "react";
-import styles from "./VideoHeader.module.scss";
-import beloe_logo from "../../icons/Header/beloe_logo.svg";
-import { useState, useEffect } from "react";
-import { Link } from "react-scroll";
+import React, { ReactElement } from 'react';
+import styles from './VideoHeader.module.scss';
+import beloe_logo from '../../icons/Header/beloe_logo.svg';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-scroll';
 
 const VideoHeader = (): ReactElement => {
   const [color, setColor] = useState(false);
@@ -14,27 +14,17 @@ const VideoHeader = (): ReactElement => {
     }
   };
 
-  window.addEventListener("scroll", changeBackground);
+  window.addEventListener('scroll', changeBackground);
 
-  const item_1: any = document.getElementById("item_1");
-  const item_2: any = document.getElementById("item_2");
-  const item_3: any = document.getElementById("item_3");
-  const item_4: any = document.getElementById("item_4");
+  const item_1: any = document.getElementById('item_1');
+  const item_2: any = document.getElementById('item_2');
+  const item_3: any = document.getElementById('item_3');
+  const item_4: any = document.getElementById('item_4');
 
   return (
-    <div
-      className={
-        color ? `${styles.wrapper} ${styles["wrapper-bg"]}` : styles.wrapper
-      }
-    >
-      <div className={styles.content}>
-        <Link
-          to="logo"
-          spy={true}
-          smooth={true}
-          duration={1000}
-          className={styles.logo}
-        >
+    <div className={color ? `${styles.wrapper} ${styles['wrapper-bg']}` : styles.wrapper}>
+      <div className={color ? `${styles.content} ${styles['content-bg']}` : styles.content}>
+        <Link to="logo" spy={true} smooth={true} duration={1000} className={styles.logo}>
           <img src={beloe_logo} alt="" />
         </Link>
 
