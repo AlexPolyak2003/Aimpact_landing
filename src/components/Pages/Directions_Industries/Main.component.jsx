be6ff41cadc83bd1fbecf1bd8e12059a34a0e571
AscentDP <podvalov.dima@mail.ru>
2023-12-03 01:17:56 +0300

    анимация

--- src/components/Pages/Directions_Industries/Main.component.jsx
+++ src/components/Pages/Directions_Industries/Main.component.jsx
@@ -1,57 +1,31 @@
 import styles from './Main.module.scss';
 import { useState, useRef } from 'react';
-import Direction_page from './Directions/Directions.component';
+import DirectionPage from './Directions/Directions.component';
 import Industry from './Industries/Industry.component';
 import mozg from '../../../icons/Directions/mozg.svg';
 
 const Main = () => {
-  const button_1 = document.getElementById('btn_1');
-  const button_2 = document.getElementById('btn_2');
   const [firstState, setFirstState] = useState(true);
   const [secondState, setSecondState] = useState(false);
+  const button_1 = useRef();
+  const button_2 = useRef();
 
-  const [isActive, setIsActive] = useState(false);
-
-  const [color, setColor] = useState(['#b0bde6', '#2d418d']);
-  const [underlineColor, setUnderlineColor] = useState(['#2d418d', '#b0bde6']);
-  const [underline, setUnderline] = useState(['underline', 'none']);
-
-  const toggleFirstContent = () => {
-    setIsActive(true);
+  const handleClick = () => {
     setFirstState(true);
     setSecondState(false);
   };
 
-  const toggleSecondContent = () => {
-    setIsActive(true);
+  const handleClick_2 = () => {
     setFirstState(false);
     setSecondState(true);
   };
 
   const InitialContent = () => {
-    return <Direction_page className={styles.hidden} />;
+    return <DirectionPage className={styles.hidden} />;
   };
 
   const ChangedContent = () => {
     return <Industry />;
-  };
-
-  const handleClick = () => {
-    if (!firstState) {
-      setColor(['#b0bde6', '#2d418d']);
-      setUnderlineColor(['#b0bde6', '#2d418d']);
-      setUnderline(['underline', 'none']);
-      toggleFirstContent();
-    }
-  };
-
-  const handleClick_2 = () => {
-    if (!secondState) {
-      setColor(['#2d418d', '#b0bde6']);
-      setUnderlineColor(['#2d418d', '#b0bde6']);
-      setUnderline(['none', 'underline']);
-      toggleSecondContent();
-    }
   };
 
   return (
@@ -60,23 +34,18 @@ const Main = () => {
         <div className={styles.left_box}>
           <div className={styles.titles}>
             <h1
-              style={{ color: color[1], 'text-decoration-line': underline[0] }}
               id="btn_1"
               ref={button_1}
               onClick={handleClick}
-              className={styles.title_1}>
+              className={firstState ? styles.active : styles.title_1}>
               направления
             </h1>
 
             <h1
-              style={{
-                color: color[0],
-                'text-decoration-line': underline[1],
-              }}
               id="btn_2"
               ref={button_2}
               onClick={handleClick_2}
-              className={styles.title_2}>
+              className={secondState ? styles.active : styles.title_2}>
               индустрии
             </h1>
           </div>
@@ -85,7 +54,8 @@ const Main = () => {
             <img src={mozg} alt="мозг" />
           </div>
         </div>
-        {secondState ? <ChangedContent /> : <Direction_page />}
+
+        {secondState ? <ChangedContent /> : <DirectionPage />}
       </div>
 
       {/* mobile */}
@@ -94,25 +64,24 @@ const Main = () => {
         <div className={styles.left_box}>
           <div className={styles.titles}>
             <h1
-              style={{ color: color[1], 'text-decoration-line': underline[0] }}
               id="btn_1"
               ref={button_1}
               onClick={handleClick}
-              className={styles.title_1}>
+              className={firstState ? styles.active : styles.title_1}>
               направления
             </h1>
 
             <h1
-              style={{
-                color: color[0],
-                'text-decoration-line': underline[1],
-              }}
               id="btn_2"
               ref={button_2}
               onClick={handleClick_2}
-              className={styles.title_2}>
+              className={secondState ? styles.active : styles.title_2}>
               индустрии
             </h1>
+          </div>
+
+          <div className={styles.mozg_position}>
+            <img src={mozg} alt="мозг" />
           </div>
         </div>
 
